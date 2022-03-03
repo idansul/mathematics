e1749c1670e4da817ade80566676a69e36d0fe7b
idansul <77149629+idansul@users.noreply.github.com>
2022-03-03 16:46:23 +0200

    Update jacobson_witt_algebra.py

--- lie_algebras/jacobson_witt_algebra.py
+++ lie_algebras/jacobson_witt_algebra.py
@@ -1,122 +1,83 @@
-import sympy
 from sympy.functions.combinatorial.factorials import binomial
-from sympy import Symbol, symbols, simplify, combsimp
-import numpy
+from sympy import Symbol, symbols
+import numpy as np
 from itertools import islice, product
 import re
+from truncated_polynomial_ring import TruncatedPolyRing
 
 x_1, x_2, D_1, D_2 = symbols("x_1, x_2, D_1, D_2")
 
 
-class JWitt:
+class JWitt(TruncatedPolyRing):
     # A class for calculations on the Jacobson-Witt Lie Algebras W(m;1) over a field of characteristic p
     
     def __init__(self, p, m):
         # Initializes the variables, derivations and basis
-        self.p = p
-        self.m = m
-        self.x = [Symbol("x_" +  str(i + 1)) for i in range(m)]
+        super().__init__(p, m)
         self.D = [Symbol("D_" + str(i + 1)) for i in range(m)]
-        self.epsilon = [Symbol("epsilon_" + str(i + 1)) for i in range(m)]
+        self.basis = self.create_basis()
+        self.dimension = m * p ** m
+    
+    def create_basis(self):
+        # Creates the basis set of the algebra
         monoms = []
+        monomials = []
         for x_i in self.x:
             monoms.append([x_i ** i for i in range(self.p)])
-        self.monomials = []
         for mon in product(*reversed(monoms)):
-            self.monomials.append(numpy.prod(mon))
-        self.basis = [monomial * D for D in self.D for monomial in self.monomials]
-        self.dimension = m * self.p ** m
+            monomials.append(np.prod(mon))
+        return [monomial * D for D in self.D for monomial in monomials]
     
-    def mod_p(self, n):
-        # Takes the Modulo p of an integer n
-        if type(n) is sympy.Integer:
-            return n % self.p
-        return n
+    def extract_derivation(self, element):
+        # Gets an element from the algebra and returns its polynomial and derivation components separately
+        if element:
+            d = eval(re.findall(f"D_[1-{self.m}]", str(element))[0])
+            return element/d, d
+        else:
+            return 0, 0
     
-    def coefs_mod_p(self, element):
-        # Operates Modulo p on the coefficients of a general element in the algebra
-        decomposed = re.split("(?=[-+])", str(element))
-        if decomposed[0] == "":
-            decomposed = decomposed[1:]
-        decomposed = list(map(eval, decomposed))
-        return sum(list(map(lambda x: numpy.prod(self.get_coef(x)), decomposed)))
+    def decompose(self, element):
+        # Decomposes an element into its coefficient, monomial exponent vector and derivation components
+        coef, element = self.get_coef(element)
+        monomial, d = self.extract_derivation(element)
+        exp = self.get_exp(monomial)
+        return coef, exp, d
     
-    def element_conversion(self, exp, d):
+    def alg_eliminate_exp_over_p(self, element):
+        # Eliminates summands with exponents bigger than p - 1
+        return sum(list(map(lambda x: x if max(self.decompose(x)[1]) < self.p else 0, self.get_summands(element))))
+    
+    def exp_to_monomial(self, exp):
         # Converts an exponent vector and a derivarion index into a corresponding element
-        x = numpy.prod(list(map(lambda i: self.x[i] ** exp[i], range(self.m))))
-        return x * self.D[d - 1]
+        return np.prod(list(map(lambda i: self.x[i] ** exp[i], range(self.m))))
 
     def component_calc(self, a, b, epsilon, d):
         # Calculates a component in the Lie bracket
-        epsilon = [0 if i + 1 != epsilon else 1 for i in range(self.m)]
+        d_index = int(str(epsilon)[-1])
+        epsilon = [0 if i + 1 != d_index else 1 for i in range(self.m)]
         n = [a[i] + b[i] - epsilon[i] for i in range(self.m)]
         if any(i < 0 or i >= self.p for i in n):
             return 0
-        binom = self.mod_p(numpy.prod([binomial(n[i], a[i]) for i in range(self.m)]))
-        return binom * self.element_conversion(n, d)
+        binom = np.prod([binomial(n[i], a[i]) for i in range(self.m)]) % self.p
+        return binom * self.exp_to_monomial(n) * d
     
     def lie_brackets(self, element1_exp, element1_d, element2_exp, element2_d):
         # Calculates the Lie brackets of 2 single elements
         return self.component_calc(element1_exp, element2_exp, element1_d, element2_d) \
                 - self.component_calc(element2_exp, element1_exp, element2_d, element1_d)
-    
-    def get_coef(self, element):
-        # Gets coefficient of an element
-        decomposed = str(element).split("*", 1)
-        if isinstance(eval(decomposed[0]), int):
-            return eval(decomposed[0]) % self.p, eval(decomposed[1])
-        elif decomposed[0][0] == "-":
-            return self.p - 1, -element
-        return 1, element
-    
-    def get_exp(self, monomial):
-        # Gets the exponent vector of a monomial
-        monomials = re.split("\*(?!\*)", str(monomial))
-        i = 0
-        powers = [0] * self.m
-        while i < len(monomials):
-            index = int(monomials[i][2]) - 1
-            if monomials[i][-1] == "*":
-                powers[index] = int(monomials[i + 1])
-                i += 2
-            else:
-                powers[index] = 1
-                i += 1
-        return powers
-    
-    def decompose(self, element):
-        # Decomposes an element into its coefficient, monomial exponent vector and derivation components
-        coef, element = self.get_coef(element)
-        decomposed = str(element).split("*", 1)
-        d = int(decomposed[0][-1])
-        if len(decomposed) == 2:
-            exp = self.get_exp(eval(decomposed[1]))
-        else:
-            exp = [0] * self.m
-        return coef, exp, d
         
-    def lie(self, A, B, params=False):
-        # Calculates the Lie brackets of 2 general element
+    def lie(self, A, B):
+        # Calculates the Lie brackets of 2 general element: [A,B]
+        A_summands = self.get_summands(A)
+        B_summands = self.get_summands(B)
         result = 0
-        for element1 in A:
+        for element1 in A_summands:
             element1_coef, element1_exp, element1_d = self.decompose(element1)
-            for element2 in B:
+            for element2 in B_summands:
                 element2_coef, element2_exp, element2_d = self.decompose(element2)
                 prod = self.lie_brackets(element1_exp, element1_d, element2_exp, element2_d) * element2_coef
                 result += prod * element1_coef
-        if result != 0 and params is False:
-            return self.coefs_mod_p(result)
-        elif params is True:
-            return result
-        return 0
-    
-    def lie_simp(self, A, B):
-        # Returns a simplified presentation of self.lie
-        return simplify(self.lie(A, B))
-    
-    def lie_combsimp(self, A, B):
-        # Returns a combinatorially simplified presentation of self.lie
-        return combsimp(self.lie(A, B))
+        return self.coefs_mod_p(result)
     
     def search_bracket(self, elements, start=1, stop=None, verbose=False, verbose_result=False, removals=[None]):
         # Searches bracket presentations for a list of elements
@@ -159,10 +120,29 @@ class JWitt:
                 if index + 1 > max(slices):
                     break
             left = [element for element in left if element != 0]
-            print(left)
             for right in islice(product(*M), 1, None):
                 right = [element for element in right if element != 0]
                 result = self.lie(left, right)
                 if result not in results.values():
                     results[(tuple(left), tuple(right))] = result
         return results
+    
+    def epsilon_variations_calc(self, exponents):
+        # Calculates [x^epsilon_i D_j, x^exp D_j] for all indices and the given exponents
+        for exp in exponents:
+            for d in product(self.D, repeat=2):
+                for index in range(self.m):
+                    epsilon = [0] * self.m
+                    epsilon[index] = 1
+                    left = self.exp_to_monomial(epsilon) * d[0]
+                    right = self.exp_to_monomial(exp) * d[1]
+                    print(f"[{str(left)}, {str(right)}] =", self.lie(left, right))
+    
+    def is_homogeneous(self, element):
+        # Checks if an element is homogeneous or not (if all its summands have the same degree)
+        summands = self.get_summands(element)
+        degree = sum(self.decompose(summands[0])[1])
+        for summand in summands[1:]:
+            if sum(self.decompose(summand)[1]) != degree:
+                return False
+        return True
